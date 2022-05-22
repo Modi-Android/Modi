18f4aeea75892bdee636e113756413bc195295b6
WooKongM <kimy5878@naver.com>
2022-05-22 22:33:09 +0900

    Modi_MW

--- app/src/main/java/com/example/modi/Project/ProjectFragment.kt
+++ app/src/main/java/com/example/modi/Project/ProjectFragment.kt
@@ -22,6 +22,7 @@ class ProjectFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
